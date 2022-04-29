e8552bb7bc872415070b7388c48f96ea9d453c3d
Shaheer <Shaheers.Bhangar@gmail.com>
2022-04-29 15:17:52 -0500

    test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded());
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs vi'ew file
+// use res.render to load up an ejs view file
 
 // login page 
 app.get('/', function(req, res) {
